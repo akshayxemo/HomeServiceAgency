307226bb89cf232005aa81c294ccd3ec04d0210d
Arijit Bose <arijitbose0007@gmail.com>
2022-12-11 20:44:09 +0530

    Feedback + GetinTouch added

--- HSA_HomeServiceAgency/src/main/webapp/Contact_us.js
+++ HSA_HomeServiceAgency/src/main/webapp/Contact_us.js
@@ -5,3 +5,12 @@ document.getElementById("loginButton").onclick = function () {
 document.getElementById("signupButton").onclick = function () {
     window.location.href = "userType.html";
 };
+
+document.getElementById("getintouchButton").onclick = function () {
+    window.location.href = "GetinTouch.html";
+};
+
+document.getElementById("submitButton").onclick = function () {
+    window.location.href = "Feedback.html";
+    
+};
