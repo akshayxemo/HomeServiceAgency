ca02f6e339273575247d3e2fe607840c81d8a2b4
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2022-12-18 15:51:53 +0530

    servicePage.js change

--- HSA_HomeServiceAgency/src/main/webapp/servicePage.js
+++ HSA_HomeServiceAgency/src/main/webapp/servicePage.js
@@ -1,4 +1,39 @@
-document.getElementById("Services").onclick = function () {
+document.getElementById("carpenter").onclick = function () {
     window.location.href = "servicePage.html";
 };
 
+document.getElementById("plumber").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("electrician").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("applienceRepair").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("houseCleaning").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("homePainting").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("saloonForMen").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("saloonForWoman").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("therapyForMen").onclick = function () {
+    window.location.href = "servicePage.html";
+};
+
+document.getElementById("therapyForWomen").onclick = function () {
+    window.location.href = "servicePage.html";
+};
